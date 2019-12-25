09001b54ccdcf6b1b76b99c0024b0ce4a8b46b05
976047167 <976047167@qq.com>
2019-12-25 15:33:24 +0800

    init

---
+++ core/card/card.ts
@@ -0,0 +1,70 @@
+export class Card {
+    public q_cache: QueryCache;
+
+}
+
+export class CardData {
+    public code: number;
+    public alias: number;
+    public setcode: number;
+    public type: number;
+    public level: number;
+    public attribute: number;
+    public race: number;
+    public attack: number;
+    public defense: number;
+    public lscale: number;
+    public rscale: number;
+    public linkMarker: number;
+    public clear() {
+    }
+}
+export class CardState {
+    public code: number;
+    public code2: number;
+    public setcode: number;
+    public type: number;
+    public level: number;
+    public rank: number;
+    public link: number;
+    public lscale: number;
+    public rscale: number;
+    public attribute: number;
+    public race: number;
+    public attack: number;
+    public defence: number;
+    public baseAttack: number;
+    public baseDefense: number;
+    public controler: number;
+    public location: number;
+    public sequence: number;
+    public position: number;
+    public reason: number;
+    public pzone: number;
+    public reasonCard: Card;
+    public reasonPlayer: number;
+    public reasonEffect: any;
+    public isLocation(loc: number): boolean {
+        return true;
+    }
+}
+
+class QueryCache {
+    public code: number;
+    public alias: number;
+    public type: number;
+    public level: number;
+    public rank: number;
+    public link: number;
+    public attribute: number;
+    public race: number;
+    public attack: number;
+    public defense: number;
+    public base_attack: number;
+    public base_defense: number;
+    public reason: number;
+    public status: number;
+    public lscale: number;
+    public rscale: number;
+    public link_marker: number;
+}
