7782003fd312a53182c578fb8dd7e15e3af9355b
976047167 <976047167@qq.com>
2019-12-30 17:10:11 +0800

    回滚工具脚本

--- core/utils1.js
+++ core/utils1.js
@@ -1,10 +1,18 @@
-function *createIterator() {
-    console.log(111111111)
-    console.log(111111111)
-    console.log(111111111)
-    console.log(111111111)
-}
-let iterator = createIterator();
-console.log(iterator.next()); // "{ value: 1, done: false }"
-console.log(iterator.return()); // "{ value: 1, done: false }"
-console.log(iterator.next()); // "{ value: undefined, done: true }"
+const fs = require('fs')
+const o= fs.readFileSync('/Users/moon/test_o','utf-8')
+const d =fs.readFileSync('/Users/moon/test_d','utf-8')
+const dess =[]
+const a=[]
+o.trim().split('\n').forEach(function(v, i) {
+    const index = v.lastIndexOf("--")
+    const des ="/**"+v.substr(index+2)+"*/"
+    dess.push(des)
+})
+o.split('\n').forEach(function(v, i) {
+    if(dess[i]){
+        console.log("\t"+dess[i])
+    }
+    const index = v.lastIndexOf("--")
+    const des =v.substr(0,index)+","
+    console.log(des)
+})
